25e216a8a0856d41ecd08213ff25e3082fc6557f
Chris Wise <cwise@murmurinformatics.com>
2011-09-20 15:33:22 -0400

    added styling for buttons etc

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,13 +1,15 @@
 function set_tag_fields() {
-  inited=false;
   $('.tag_field').tagit({
     allowSpaces: true, 
     removeConfirmation: true
-  });
-  inited=true;      
+  });   
 };
 
+function set_buttons() {
+  $('.button').button();
+};
 
 $(function() {
 	set_tag_fields();
+	set_buttons();
 });
