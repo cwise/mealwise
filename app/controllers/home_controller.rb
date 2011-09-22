334561aec22e13ca048fcaf875960862ffcd7573
Chris Wise <cwise@murmurinformatics.com>
2011-09-22 15:18:16 -0400

    added name to user; added name to entry forms; name is on listing of meals

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @tags=Meal.tag_counts_on(:tags)
+    @tags.sort!{ |a,b| a.name.downcase <=> b.name.downcase }
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110920191210) do
+ActiveRecord::Schema.define(:version => 20110922191454) do
 
   create_table "meals", :force => true do |t|
     t.integer  "user_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(:version => 20110920191210) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
