9ec9c74adfd946543930fb3591a70f8d6d40ef88
Chris Wise <cwise@murmurinformatics.com>
2013-01-24 12:01:41 -0500

    added a last served for meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,6 +1,10 @@
 class Meal < ActiveRecord::Base
   acts_as_taggable
-  has_and_belongs_to_many :served_meals
+  has_and_belongs_to_many :served_meals, :order => 'served_at'
   belongs_to :user
   validates :name, :presence => true, :uniqueness => true
+
+  def last_served
+    served_meals.last.try(:served_at)
+  end
 end
